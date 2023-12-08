8a4d57973410ad4a8442e9867e2a134df7c51ed1
Austin Williams <brock_williams@brown.edu>
2023-12-08 16:52:00 -0500

    feat: set up README

--- Frontend/mock/src/components/PreferenceTicker.tsx
+++ Frontend/mock/src/components/PreferenceTicker.tsx
@@ -4,10 +4,10 @@ import { ControlledInput } from "./ControlledInput";
 import { ResortDropdown } from "./ResortsDropdown";
 
 interface PreferenceTickerProps {
-  preferenceMap: Map<string, number>;
-  setPreferenceMap: Dispatch<SetStateAction<Map<string, number>>>;
-  setReset: Dispatch<number>;
-  preference: string;
+	preferenceMap: Map<string, number>;
+	setPreferenceMap: Dispatch<SetStateAction<Map<string, number>>>;
+	setReset: Dispatch<number>;
+	preference: string;
 }
 
 var resetNum = 1;
@@ -20,67 +20,63 @@ var resetNum = 1;
  * @returns
  */
 export function PreferenceTicker(props: PreferenceTickerProps) {
-  function up() {
-    var currentPref = props.preferenceMap.get(props.preference);
-    if (currentPref === undefined) {
-      throw console.error();
-    }
-    if (currentPref < 10) {
-      props.setPreferenceMap(
-        props.preferenceMap.set(props.preference, currentPref + 1)
-      );
-    }
-    props.setReset(resetNum);
-    resetNum += 1;
-  }
-  function down() {
-    var currentPref = props.preferenceMap.get(props.preference);
-    if (currentPref === undefined) {
-      throw console.error();
-    }
-    if (currentPref > 0) {
-      props.setPreferenceMap(
-        props.preferenceMap.set(props.preference, currentPref - 1)
-      );
-    }
-    props.setReset(resetNum);
-    resetNum += 1;
-  }
-  return (
-    <div>
-      <table>
-        <tr>
-          <td>
-            <p>{props.preferenceMap.get(props.preference)}</p>
-          </td>
-          <td>
-            <table>
-              <tr>
-                <td>
-                  <button
-                    id="upButton"
-                    onClick={() => up()}
-                    aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
-                  >
-                    ^
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <button
-                    id="downButton"
-                    onClick={() => down()}
-                    aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
-                  >
-                    v
-                  </button>
-                </td>
-              </tr>
-            </table>
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
+	function up() {
+		var currentPref = props.preferenceMap.get(props.preference);
+		if (currentPref === undefined) {
+			throw console.error();
+		}
+		if (currentPref < 10) {
+			props.setPreferenceMap(props.preferenceMap.set(props.preference, currentPref + 1));
+		}
+		props.setReset(resetNum);
+		resetNum += 1;
+	}
+	function down() {
+		var currentPref = props.preferenceMap.get(props.preference);
+		if (currentPref === undefined) {
+			throw console.error();
+		}
+		if (currentPref > 0) {
+			props.setPreferenceMap(props.preferenceMap.set(props.preference, currentPref - 1));
+		}
+		props.setReset(resetNum);
+		resetNum += 1;
+	}
+	return (
+		<div>
+			<table>
+				<tr>
+					<td>
+						<p>{props.preferenceMap.get(props.preference)}</p>
+					</td>
+					<td>
+						<table>
+							<tr>
+								<td>
+									<button
+										id="upButton"
+										onClick={() => up()}
+										aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
+									>
+										^
+									</button>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<button
+										id="downButton"
+										onClick={() => down()}
+										aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
+									>
+										v
+									</button>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
 }
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # term-project-tbzhao-tpezza-smschuch-bwilli48-
+
+## Project details
+
+### Team members and contributions
+
+tpezza - things done - X hours
+
+tbzhao - things done - X hours
+
+bwilli48 - things done - X hours
+
+smschuch - things done - X hours
+
+Link to repo: https://github.com/cs0320-f23/term-project-tbzhao-tpezza-smschuch-bwilli48
+
+## Design choices -- high level design of your program
+
+## Errors/Bugs
+
+## Tests/Functionality
+
+### Frontend
+
+### Backend
+
+## Running
+
+### Frontend
+
+For the frontend. We have the all the command functionality found in the previous sprints (loading, viewing, and parsing a CSV; broadband queries with the ACSAPI) as well as one new command desc_search, which takes a description of the required component and runs a queried search for the data on the backend. This in turn highlights the HOLC regions that contain this keyword, which are represented by making them far more opaque that their non-highlighted counterparts.
+
+For running the frontend, please run the following commands:
+
+- `cd frontend` -> moves you to the correct directory
+- `npm install` -> installs npm, which we need to run the frontend
+- `npm playwright install` -> installs playwright, which runs our tests for us
+- `npm run dev` -> runs the server
+
+### Backend
