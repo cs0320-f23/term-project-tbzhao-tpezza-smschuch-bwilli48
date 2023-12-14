b7582ca5e253dece61b9ef22e894a0293069574e
tyypezza <tyler_pezza@brown.edu>
2023-12-13 20:54:21 -0500

    Feat: Added the agreed on attributed

--- Frontend/mock/src/components/ResortsList.tsx
+++ Frontend/mock/src/components/ResortsList.tsx
@@ -5,23 +5,23 @@ import "../styles/main.css";
  * Props for the resorts list. Includes just an array of the resorts.
  */
 interface ResortsListProps {
-	resortList: string[];
+  resortList: string[];
 }
 
 /**
  * Function that represents the list of resorts. Functions similiarly to a REPL history box.
  */
 export function ResortsList(props: ResortsListProps) {
-	return (
-		<div
-			id="listDiv"
-			className="repl-history"
-			aria-label="History Section, which lists the outputs of your previous commands"
-		>
-			<h1>Resorts</h1>
-			{props.resortList.map((command) => (
-				<div>{command}</div>
-			))}
-		</div>
-	);
+  return (
+    <div
+      id="listDiv"
+      className="repl-history"
+      aria-label="History Section, which lists the outputs of your previous commands"
+    >
+      <h1>Resorts</h1>
+      {props.resortList.map((resort) => (
+        <div>{resort}</div>
+      ))}
+    </div>
+  );
 }
--- Frontend/mock/src/components/Sort.tsx
+++ Frontend/mock/src/components/Sort.tsx
@@ -10,8 +10,8 @@ import { sortResorts } from "./OrderResorts";
  * Props for the sort function. Includes the array of resorts and a setter for said array.
  */
 interface SortProps {
-	resortList: string[];
-	setResortList: Dispatch<SetStateAction<string[]>>;
+  resortList: string[];
+  setResortList: Dispatch<SetStateAction<string[]>>;
 }
 
 /**
@@ -19,89 +19,102 @@ interface SortProps {
  * sort methods and a button that performs the sorting.
  */
 export function Sort(props: SortProps) {
-	// State for whether to show the dropdown menu.
-	const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  // State for whether to show the dropdown menu.
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
 
-	// State for the text in the select box.
-	const [selectSort, setSelectSort] = useState<string>("");
+  // State for the text in the select box.
+  const [selectSort, setSelectSort] = useState<string>("");
 
-	// The options that users can sort by.
-	const sortOptions = () => {
-		return ["Recent Snowfall", "Vertical Drop", "Trail Count", "Ticket Cost", "Skiable Acres"];
-	};
+  // The options that users can sort by.
+  const sortOptions = () => {
+    return [
+      "Snowfall Amount",
+      "Last Snowfall",
+      "Base-depth",
+      "Price",
+      "Lifts Open",
+      "Summit Elevation",
+      "Temperature",
+      "Windspeed",
+    ];
+  };
 
-	/**
-	 * Toggle the select drop down menu
-	 */
-	const toggleDropDown = () => {
-		setShowDropDown(!showDropDown);
-	};
+  /**
+   * Toggle the select drop down menu
+   */
+  const toggleDropDown = () => {
+    setShowDropDown(!showDropDown);
+  };
 
-	/**
-	 * Handler that hides the dropdown menu if a click occurs outside of the dropdown element.
-	 * @param event The mouse event
-	 */
-	const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
-		if (event.currentTarget === event.target) {
-			setShowDropDown(false);
-		}
-	};
+  /**
+   * Handler that hides the dropdown menu if a click occurs outside of the dropdown element.
+   * @param event The mouse event
+   */
+  const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
+    if (event.currentTarget === event.target) {
+      setShowDropDown(false);
+    }
+  };
 
-	/**
-	 * Callback function to consume the sort option
-	 * @param sortOption The selected options
-	 */
-	const sortOptionsSelection = (sortOption: string): void => {
-		setSelectSort(sortOption);
-	};
+  /**
+   * Callback function to consume the sort option
+   * @param sortOption The selected options
+   */
+  const sortOptionsSelection = (sortOption: string): void => {
+    setSelectSort(sortOption);
+  };
 
-	/**
-	 * Function that updates the resort list when the submut button is clicked
-	 * @param commandString input by the user currently in the box when submit is clicked
-	 */
-	function handleSubmit() {
-		props.setResortList(sortResorts(selectSort));
-	}
+  /**
+   * Function that updates the resort list when the submut button is clicked
+   * @param commandString input by the user currently in the box when submit is clicked
+   */
+  function handleSubmit() {
+    props.setResortList(sortResorts(selectSort));
+  }
 
-	return (
-		//actually returning the input html dev thing
-		<div
-			className="repl-input"
-			aria-label="Input section, which contains a input box where you can enter the commands you wish to run."
-		>
-			<table>
-				<tr>
-					<td className="sortTableDatum">
-						<button
-							id="sortButton"
-							onClick={() => handleSubmit()}
-							aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
-						>
-							Sort
-						</button>
-					</td>
-					<td className="sortTableDatum">
-						<div>
-							<button
-								id="sortDropdown"
-								className={showDropDown ? "active" : undefined}
-								onClick={(): void => toggleDropDown()}
-								onBlur={(e: React.FocusEvent<HTMLButtonElement>): void => dismissHandler(e)}
-							>
-								<div>{selectSort ? "Sort by: " + selectSort : "Select ..."} </div>
-								{showDropDown && (
-									<SortDropdown
-										sortOptions={sortOptions()}
-										showDropDown={false}
-										toggleDropDown={(): void => toggleDropDown()}
-										sortOptionsSelection={sortOptionsSelection}
-									/>
-								)}
-							</button>
-						</div>
-					</td>
-				</tr>
-			</table>
-		</div>
-	);
+  return (
+    //actually returning the input html dev thing
+    <div
+      className="repl-input"
+      aria-label="Input section, which contains a input box where you can enter the commands you wish to run."
+    >
+      <table>
+        <tr>
+          <td className="sortTableDatum">
+            <button
+              id="sortButton"
+              onClick={() => handleSubmit()}
+              aria-label="Submit button. Click the Submit button with the mouse or click the return key on your keyboard to enter the command you entered in the input box"
+            >
+              Sort
+            </button>
+          </td>
+          <td className="sortTableDatum">
+            <div>
+              <button
+                id="sortDropdown"
+                className={showDropDown ? "active" : undefined}
+                onClick={(): void => toggleDropDown()}
+                onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
+                  dismissHandler(e)
+                }
+              >
+                <div>
+                  {selectSort ? "Sort by: " + selectSort : "Select ..."}{" "}
+                </div>
+                {showDropDown && (
+                  <SortDropdown
+                    sortOptions={sortOptions()}
+                    showDropDown={false}
+                    toggleDropDown={(): void => toggleDropDown()}
+                    sortOptionsSelection={sortOptionsSelection}
+                  />
+                )}
+              </button>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
