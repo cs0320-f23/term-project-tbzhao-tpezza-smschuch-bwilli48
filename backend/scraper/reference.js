274d67f087d57eb38b7021f34e8a3d6921f79e99
tonybzhao <113156199+tonybzhao@users.noreply.github.com>
2023-12-16 20:30:38 -0500

    Update

--- backend/scraper/reference.js
+++ backend/scraper/reference.js
@@ -1,62 +1,68 @@
-import puppeteer from "puppeteer";
-
-// TODO: Now it's your turn to improve the scraper and make him get more data from the Quotes to Scrape website.
-// Here's a list of potential improvements you can make:
-// - Navigate between all pages using the "Next" button and fetch the quotes on all the pages
-// - Fetch the quote's tags (each quote has a list of tags)
-// - Scrape the author's about page (by clicking on the author's name on each quote)
-// - Categorize the quotes by tags or authors (it's not 100% related to the scraping itself, but that can be a good improvement)
-
-const getQuotes = async () => {
-  // Start a Puppeteer session with:
-  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-
-  // Open a new page
-  const page = await browser.newPage();
-
-  // On this new page:
-  // - open the "http://quotes.toscrape.com/" website
-  // - wait until the dom content is loaded (HTML is ready)
-  await page.goto("http://quotes.toscrape.com/", {
-    waitUntil: "domcontentloaded",
-  });
-
-  while( page.evaluate(() => {
-    document.querySelector(".pager > .next > a")}
-    )){
-
-  // Get page data
-  const quotes = await page.evaluate(() => {
-    // Fetch the first element with class "quote"
-    // Get the displayed text and returns it
-    const quoteList = document.querySelectorAll(".quote");
-    console.log(typeof(quoteList));
-    // Convert the quoteList to an iterable array
-    // For each quote fetch the text and author
-    return Array.from(quoteList).map((quote) => {
-      // Get the sub-elements from the previously fetched quote element
-      const text = quote.querySelector(".text").innerText;
-      const author = quote.querySelector(".author").innerText;
-
-      return { text, author };
+const getResorts = async () => {
+    // Start a Puppeteer session with:
+    // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+    // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
     });
-  });
-
-  // Display the quotes
-  console.log(quotes);
-
-  // Click on the "Next page" button
-  await page.click(".pager > .next > a");
-  await page.waitForTimeout(1000);
-}
-  // Close the browser
-  await browser.close();
-};
-
-// Start the scraping
-getQuotes();
+  
+    // Open a new page
+    const page = await browser.newPage();
+    const allResorts = [];
+  
+    // On this new page:
+    // - wait until the dom content is loaded (HTML is ready)
+   // for (let index = 0; index < 33; index++) {
+      for (let index = 0; index < 3; index++) {
+      if(index == 0){
+          let link = "sorted/number-lifts/";
+          await page.goto("https://www.skiresort.info/ski-resorts/" + link , {
+      waitUntil: "domcontentloaded",
+    });
+  
+      } else{
+          let link = "page/" + index.toString() + "/sorted/number-lifts/";
+          await page.goto("https://www.skiresort.info/ski-resorts/" + link , {
+      waitUntil: "domcontentloaded",
+    });
+  
+      }
+  
+  
+   // while( page.evaluate(() => {
+    //  document.querySelector(".pager > .next > a")}
+     // )){
+  
+    // Get page data
+    const resorts = await page.evaluate(() => {
+          // Get the displayed text and returns it
+      const resortList = document.querySelectorAll(".panel.panel-default.resort-list-item.resort-list-item-image--big");
+      console.log(resortList);
+      
+  
+      return Array.from(resortList).map((resort) => {
+        const name = resort.querySelector(".h3").innerText;
+        console.log(name);
+        const lifts = resort.querySelector(".inline-dot").innerText;
+        console.log(lifts);
+        
+        return {name, lifts};
+      });
+    });
+  
+    // Display the quotes
+    //console.log(resorts);
+    allResorts.push(...resorts);
+  
+    // Click on the "Next page" button
+    await page.waitForTimeout(500);
+      }
+      console.log(allResorts);
+  }
+    // Close the browser
+    //await browser.close();
+  //};
+  
+  // Start the scraping
+      var json = JSON.stringify(getResorts());
