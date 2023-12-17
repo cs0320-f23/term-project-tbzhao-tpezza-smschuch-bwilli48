1757ae75947f78a658f4f43eaa0f76f50a865b94
tyypezza <tyler_pezza@brown.edu>
2023-12-16 22:41:19 -0500

    Feat: mocking done

--- Frontend/ski/src/components/prefs/PreferenceTicker.tsx
+++ Frontend/ski/src/components/prefs/PreferenceTicker.tsx
@@ -4,10 +4,11 @@ import { PreferenceAndValue } from "./Preferences";
 
 // Properties for the PreferenceTicker component.
 interface PreferenceTickerProps {
-	preferenceMap: Map<string, PreferenceAndValue>;
-	setPreferenceMap: Dispatch<SetStateAction<Map<string, PreferenceAndValue>>>;
-	setReset: Dispatch<number>;
-	preference: string;
+  preferenceMap: Map<string, PreferenceAndValue>;
+  setPreferenceMap: Dispatch<SetStateAction<Map<string, PreferenceAndValue>>>;
+  setReset: Dispatch<number>;
+  preference: string;
+  valueJump: number;
 }
 
 var resetNum = 1;
@@ -17,126 +18,126 @@ var resetNum = 1;
  * Renders a set of controls for adjusting these attributes associated with a user's preference.
  */
 export function PreferenceTicker(props: PreferenceTickerProps) {
-	// Manages the up arrow. Increases the count by 1 if it isn't at the max value already.
-	function upWeight() {
-		var currentPref = props.preferenceMap.get(props.preference);
-		if (currentPref === undefined) {
-			throw console.error();
-		}
-		if (currentPref.weight < 10) {
-			const newMap = new Map(props.preferenceMap);
-			newMap.get(props.preference)?.upWeight();
-			props.setPreferenceMap(newMap);
-		}
-		props.setReset(resetNum);
-		resetNum += 1;
-	}
+  // Manages the up arrow. Increases the count by 1 if it isn't at the max value already.
+  function upWeight() {
+    var currentPref = props.preferenceMap.get(props.preference);
+    if (currentPref === undefined) {
+      throw console.error();
+    }
+    if (currentPref.weight < 10) {
+      const newMap = new Map(props.preferenceMap);
+      newMap.get(props.preference)?.upWeight();
+      props.setPreferenceMap(newMap);
+    }
+    props.setReset(resetNum);
+    resetNum += 1;
+  }
 
-	// Manages the down arrow. Decreases the count by 1 if it isn't at the max.
-	function downWeight() {
-		var currentPref = props.preferenceMap.get(props.preference);
-		if (currentPref === undefined) {
-			throw console.error();
-		}
-		if (currentPref.weight > 0) {
-			const newMap = new Map(props.preferenceMap);
-			newMap.get(props.preference)?.downWeight();
-			props.setPreferenceMap(newMap);
-		}
-		props.setReset(resetNum);
-		resetNum += 1;
-	}
+  // Manages the down arrow. Decreases the count by 1 if it isn't at the max.
+  function downWeight() {
+    var currentPref = props.preferenceMap.get(props.preference);
+    if (currentPref === undefined) {
+      throw console.error();
+    }
+    if (currentPref.weight > 0) {
+      const newMap = new Map(props.preferenceMap);
+      newMap.get(props.preference)?.downWeight();
+      props.setPreferenceMap(newMap);
+    }
+    props.setReset(resetNum);
+    resetNum += 1;
+  }
 
-	// Manages the up arrow. Increases the count by 1 if it isn't at the max
-	function upValue() {
-		var currentPref = props.preferenceMap.get(props.preference);
-		if (currentPref === undefined) {
-			throw console.error();
-		}
-		if (currentPref.value < 10) {
-			const newMap = new Map(props.preferenceMap);
-			newMap.get(props.preference)?.upValue();
-			props.setPreferenceMap(newMap);
-		}
-		props.setReset(resetNum);
-		resetNum += 1;
-	}
+  // Manages the up arrow. Increases the count by 1 if it isn't at the max
+  function upValue() {
+    var currentPref = props.preferenceMap.get(props.preference);
+    if (currentPref === undefined) {
+      throw console.error();
+    }
+    const newMap = new Map(props.preferenceMap);
+    newMap.get(props.preference)?.upValue(props.valueJump);
+    props.setPreferenceMap(newMap);
+    props.setReset(resetNum);
+    resetNum += 1;
+  }
 
-	// Manages the down arrow. Decreases the count by 1 if it isn't at the max
-	function downValue() {
-		var currentPref = props.preferenceMap.get(props.preference);
-		if (currentPref === undefined) {
-			throw console.error();
-		}
-		if (currentPref.value > 0) {
-			const newMap = new Map(props.preferenceMap);
-			newMap.get(props.preference)?.downValue();
-			props.setPreferenceMap(newMap);
-		}
-		props.setReset(resetNum);
-		resetNum += 1;
-	}
+  // Manages the down arrow. Decreases the count by 1 if it isn't at the max
+  function downValue() {
+    var currentPref = props.preferenceMap.get(props.preference);
+    if (currentPref === undefined) {
+      throw console.error();
+    }
+    const newMap = new Map(props.preferenceMap);
+    newMap.get(props.preference)?.downValue(props.valueJump);
+    props.setPreferenceMap(newMap);
+    props.setReset(resetNum);
+    resetNum += 1;
+  }
 
-	return (
-		<div>
-			<table>
-				<tr>
-					<td>
-						<p>Weight: {props.preferenceMap.get(props.preference)?.weight}</p>
-					</td>
-					<td>
-						<table>
-							<tr>
-								<td>
-									<button
-										id="upButton"
-										onClick={() => upWeight()}
-										aria-label={`Increase weight for ${props.preference}`}
-									>
-										^
-									</button>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<button
-										id="downButton"
-										onClick={() => downWeight()}
-										aria-label={`Decrease weight for ${props.preference}`}
-									>
-										v
-									</button>
-								</td>
-							</tr>
-						</table>
-					</td>
-					<td>
-						<p>Value: {props.preferenceMap.get(props.preference)?.value}</p>
-					</td>
-					<td>
-						<table>
-							<tr>
-								<td>
-									<button id="upButton" onClick={() => upValue()} aria-label={`Increase value for ${props.preference}`}>
-										^
-									</button>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<button
-										id="downButton"
-										onClick={() => downValue()}
-										aria-label={`Decrease value for ${props.preference}`}
-									>
-										v
-									</button>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-		</div>
-	);
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>
+            <p>Weight: {props.preferenceMap.get(props.preference)?.weight}</p>
+          </td>
+          <td>
+            <table>
+              <tr>
+                <td>
+                  <button
+                    id="upButton"
+                    onClick={() => upWeight()}
+                    aria-label={`Increase weight for ${props.preference}`}
+                  >
+                    ^
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button
+                    id="downButton"
+                    onClick={() => downWeight()}
+                    aria-label={`Decrease weight for ${props.preference}`}
+                  >
+                    v
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <p>Value: {props.preferenceMap.get(props.preference)?.value}</p>
+          </td>
+          <td>
+            <table>
+              <tr>
+                <td>
+                  <button
+                    id="upButton"
+                    onClick={() => upValue()}
+                    aria-label={`Increase value for ${props.preference}`}
+                  >
+                    ^
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button
+                    id="downButton"
+                    onClick={() => downValue()}
+                    aria-label={`Decrease value for ${props.preference}`}
+                  >
+                    v
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- Frontend/ski/src/components/resorts/ResortClass.tsx
+++ Frontend/ski/src/components/resorts/ResortClass.tsx
@@ -4,101 +4,168 @@ import { PreferenceAndValue } from "../prefs/Preferences";
  * Represents a resort with various attributes like snowfall amount, price, etc.
  */
 export class Resort {
-	name: string;
-	snowfallAmount: number;
-	lastSnowfall: number;
-	baseDepth: number;
-	price: number;
-	liftsOpen: number;
-	summitElevation: number;
-	temperature: number;
-	windspeed: number;
+  name: string;
+  snowfallAmount: number;
+  lastSnowfall: number;
+  baseDepth: number;
+  price: number;
+  liftsOpen: number;
+  summitElevation: number;
+  temperature: number;
+  windspeed: number;
 
-	constructor(
-		name: string,
-		snowfallAmount: number,
-		lastSnowfall: number,
-		baseDepth: number,
-		price: number,
-		liftsOpen: number,
-		summitElevation: number,
-		temperature: number,
-		windspeed: number
-	) {
-		this.name = name;
-		this.snowfallAmount = snowfallAmount;
-		this.lastSnowfall = lastSnowfall;
-		this.baseDepth = baseDepth;
-		this.price = price;
-		this.liftsOpen = liftsOpen;
-		this.summitElevation = summitElevation;
-		this.temperature = temperature;
-		this.windspeed = windspeed;
-	}
+  constructor(
+    name: string,
+    snowfallAmount: number,
+    lastSnowfall: number,
+    baseDepth: number,
+    price: number,
+    liftsOpen: number,
+    summitElevation: number,
+    temperature: number,
+    windspeed: number
+  ) {
+    this.name = name;
+    this.snowfallAmount = snowfallAmount;
+    this.lastSnowfall = lastSnowfall;
+    this.baseDepth = baseDepth;
+    this.price = price;
+    this.liftsOpen = liftsOpen;
+    this.summitElevation = summitElevation;
+    this.temperature = temperature;
+    this.windspeed = windspeed;
+  }
 }
 
 // Mock data and utility functions:
 
 export const MockA = new Resort("Mock A", 12, 4, 71, 100, 4, 9000, 12, 21);
-export const MockB = new Resort("Mock B", 11, 4, 56, 120, 6, 10000, 15, 30);
+export const MockB = new Resort("Mock B", 11, 4, 56, 60, 6, 10000, 15, 30);
 export const MockC = new Resort("Mock C", 20, 1, 90, 100, 7, 4612, 10, 5);
 export const MockD = new Resort("Mock D", 90, 3, 120, 125, 18, 12500, 30, 11);
 export const MockE = new Resort("Mock E", 3, 9, 15, 140, 25, 7129, 40, 2);
 export const MockF = new Resort("Mock F", 17, 3, 40, 190, 7, 6732, 31, 18);
-export const MockG = new Resort("Mock G", 17, 2, 60, 89, 19, 13789, 13, 31);
-export const MockH = new Resort("Mock H", 71, 0, 101, 120, 8, 11771, 16, 11);
+export const MockG = new Resort("Mock G", 17, 2, 60, 80, 19, 13789, 13, 31);
+export const MockH = new Resort("Mock H", 71, 0, 101, 110, 8, 11771, 16, 11);
 
-export const mockResorts = [MockA, MockB, MockD, MockC, MockE, MockF, MockG, MockH];
+export const mockResorts = [
+  MockA,
+  MockB,
+  MockD,
+  MockC,
+  MockE,
+  MockF,
+  MockG,
+  MockH,
+];
 
-export const mockResortsSort = [MockE, MockF, MockG, MockH, MockA, MockB, MockD, MockC];
+export const mockResortsSort = [
+  MockE,
+  MockF,
+  MockG,
+  MockH,
+  MockA,
+  MockB,
+  MockD,
+  MockC,
+];
 
 export const mockResortsSearch = [MockC];
 
 export const mockResortsPref = [MockH, MockA, MockB, MockD, MockC];
 
 export function getStartResorts(): Resort[] {
-	return [];
+  return [];
 }
 
 export function getSearchResort(name: string): Resort[] {
-	return [];
+  return [];
 }
 
 export function getSortedResorts(attribute: string): Resort[] {
-	return [];
+  return [];
 }
 
-export function getRankedResorts(prefs: Map<string, PreferenceAndValue>): Resort[] {
-	return [];
+export function getRankedResorts(
+  prefs: Map<string, PreferenceAndValue>
+): Resort[] {
+  return [];
 }
 
 export function getMockStartResorts(): Resort[] {
-	return mockResorts;
+  return mockResorts;
 }
 
 export function getMockSearchResort(name: string): Resort[] {
-	mockResorts.forEach((resort) => {
-		if (resort.name === "name") {
-			return [resort];
-		}
-	});
-	return [new Resort("Resort Not Found", 0, 0, 0, 0, 0, 0, 0, 0)];
+  mockResorts.forEach((resort) => {
+    if (resort.name === "name") {
+      return [resort];
+    }
+  });
+  return [new Resort("Resort Not Found", 0, 0, 0, 0, 0, 0, 0, 0)];
 }
 
 export function getMockSortedResorts(attribute: string): Resort[] {
-	return mockResortsSort;
+  if (attribute === "Snowfall Amount") {
+    return [MockD, MockH, MockC, MockF, MockG, MockA, MockB, MockE];
+  } else if (attribute === "Last Snowfall") {
+    return [MockH, MockC, MockG, MockD, MockF, MockA, MockB, MockE];
+  } else if (attribute === "Base-Depth") {
+    return [MockD, MockH, MockC, MockA, MockG, MockB, MockF, MockE];
+  } else if (attribute === "Price") {
+    return [MockB, MockG, MockA, MockC, MockH, MockD, MockE, MockF];
+  } else if (attribute === "Lifts Open") {
+    return [MockA, MockB, MockC, MockF, MockH, MockD, MockG, MockE];
+  } else if (attribute === "Summit Elevation") {
+    return [MockC, MockF, MockE, MockA, MockB, MockH, MockD, MockG];
+  } else if (attribute === "Temperature") {
+    return [MockE, MockF, MockD, MockH, MockB, MockG, MockA, MockC];
+  } else if (attribute === "Windspeed") {
+    return [MockG, MockB, MockA, MockF, MockD, MockH, MockC, MockE];
+  } else {
+    return [new Resort("Resort Not Found", 0, 0, 0, 0, 0, 0, 0, 0)];
+  }
 }
 
-export function getMockRankedResorts(prefs: Map<string, PreferenceAndValue>): Resort[] {
-	return mockResortsPref;
+export function getMockRankedResorts(
+  prefs: Map<string, PreferenceAndValue>
+): Resort[] {
+  var sf = prefs.get("Snowfall Amount")?.value;
+  if (sf !== undefined) {
+    if (sf > 10) {
+      return [MockD, MockH, MockC, MockF, MockG, MockA, MockB, MockE];
+    }
+  }
+  var p = prefs.get("Price")?.weight;
+  if (p !== undefined) {
+    if (p > 7) {
+      return [MockB, MockG, MockA, MockC, MockH, MockD, MockE, MockF];
+    }
+  }
+  var lo = prefs.get("Lifts Open")?.weight;
+  if (lo !== undefined) {
+    if (lo > 9) {
+      return [MockA, MockB, MockC, MockF, MockH, MockD, MockG, MockE];
+    }
+  }
+  return mockResortsPref;
 }
 
 // Returns a list of resort options
 export const resortNames = () => {
-	return ["Araphoe Basin"];
+  return ["Araphoe Basin"];
 };
 
 // Returns a list of resort options
 export const mockResortNames = () => {
-	return ["Mock A", "Mock B", "Mock C", "Mock D", "Mock E", "Mock F", "Mock G", "Mock H"];
+  return [
+    "Mock A",
+    "Mock B",
+    "Mock C",
+    "Mock D",
+    "Mock E",
+    "Mock F",
+    "Mock G",
+    "Mock H",
+  ];
 };
